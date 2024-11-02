4fc015bb0cb150d53ddf5a666c5d611e1c135eeb
eumichelems <michelemsilva28@gmail.com>
2024-11-02 00:16:07 -0300

    mimi commitando

--- pataMagica/src/App.jsx
+++ pataMagica/src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
       <h1>Berlim esteve aqui</h1>
       <h2>Fernanda também.</h2>
+      <h2>Mimi também</h2>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
