eeebaeb2500cc4b33762397d98ba4d5c44771f98
nandacpc <nandacpc@gmail.com>
2024-11-04 01:16:51 -0300

    update de css

--- pataMagica/src/pages/carrinho/Carrinho.jsx
+++ pataMagica/src/pages/carrinho/Carrinho.jsx
@@ -18,22 +18,38 @@ export function CarrinhoPage() {
   }, [itensCarrinho, calcularValorTotal]);
 
   return (
+    <>
+    <div className={styles.carrinhoTitulo}><p>Meu Carrinho</p></div>
     <div className={styles.container}>
-      <h1 className={styles.carrinhoTitulo}>Meu Carrinho</h1>
-      <ul>
+      <table>
+        <tr>
+          <th className={styles.descricao}>Itens no carrinho</th>
+          <th
+          className={styles.quantidade}>Quantidade</th>
+          <th>Preço</th>
+        </tr>          
         {itensCarrinho.map((item) => (
-          <li className={styles.carrinhoItens} key={item.id}>
-            <p>
-              {item.nome} - {item.quantidade} x R${item.valorUnitario}
-            </p>
-            <button onClick={() => adicionarItens(item)}>+</button>
-            <button onClick={() => removerUmItem(item.id)}>-</button>
-            <button onClick={() => removerItens(item.id)}>X</button>
-          </li>
+        <tr key={item.id}>
+          <td className={styles.descricao}>
+            <img src={item.imagem}/>            
+              {item.nome}
+          </td>
+          <td className={styles.quantidade}>
+            <button onClick={() => adicionarItens(item)} className={styles.botaoMais}>+</button>
+            <span>{item.quantidade}</span>
+            <button onClick={() => removerUmItem(item.id)} className={styles.botaoMenos}>-</button>
+            <button onClick={() => removerItens(item.id)} className={styles.botaoX}>X</button>
+          </td>
+          <td className={styles.preco}>
+            R${item.valorUnitario}
+          </td>            
+        </tr>
+
         ))}
-      </ul>
-      <h2>Valor Total: R${valorTotal}</h2>
-      <button onClick={limparCarrinho}>Limpar</button>
+      </table>
+      <p className={styles.total}>Valor Total: R${valorTotal}</p>
+      <button onClick={limparCarrinho} className={styles.botaoLimpar}>Limpar Carrinho</button>
     </div>
+    </>
   );
 }
